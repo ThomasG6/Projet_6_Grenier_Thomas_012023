a29731f0f519164c6d6140e758e58eeda53820ab
Thomas Grenier <grenier.tjm@gmail.com>
2023-03-03 04:21:33 +0100

    add token system

---
+++ backend/middleware/auth.js
@@ -0,0 +1,16 @@
+const jsonWebToken = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jsonWebToken.verify(token, process.env.USER_KEY);
+        const userId = decodedToken.userId;
+        req.auth = {
+            userId: userId
+        };
+        next();
+    }catch(error) {
+        res.status(401).json({error});
+    }
+};
