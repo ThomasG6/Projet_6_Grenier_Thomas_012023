a002068651a177abbd7175d1448e7638815f5fd9
Thomas Grenier <grenier.tjm@gmail.com>
2023-03-03 04:16:21 +0100

    add  login for user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -22,3 +22,30 @@ exports.signup = (req, res) => {
         .catch(error => res.status(500).json({ error:error }));
 };
 
+exports.login = (req, res) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (user === null) {
+                res.status(401).json({ message: 'indentifiant/mot de passe incorrecte' });
+            }else{
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            res.status(401).json({ message: 'indentifiant/mot de passe incorrecte' });
+                        } else {
+                            console.log({ user })
+                            res.status(200).json({
+                                userId: user._id,
+                                token: jsonWebToken.sign(
+                                    { userId : user._id },
+                                    process.env.USER_KEY,
+                                    { expiresIn : '24h' }
+                                )
+                            });
+                        }
+                    })
+                    .catch(error => res.status(500).json({ error:error }));
+            }
+        })
+        .catch(error => res.status(500).json({ error:error }));
+};
