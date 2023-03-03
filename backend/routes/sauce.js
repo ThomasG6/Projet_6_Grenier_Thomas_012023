24e70e979fd9b169350a3eb737169dfdc735bc54
Thomas Grenier <grenier.tjm@gmail.com>
2023-03-03 04:11:34 +0100

    create routes

---
+++ backend/routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const router = express.Router();
+const multer = require('../middleware/multer-config');
+
+const sauceController = require('../controllers/sauce');
+
+router.post('/', auth, multer, sauceController.createSauce);
+router.put('/:id', auth, multer, sauceController.modifySauce);
+router.delete('/:id', auth, sauceController.deleteSauce);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.get('/', auth, sauceController.getAllSauces);
+router.post('/:id/like', auth, sauceController.likeUser);
+
+module.exports = router;
